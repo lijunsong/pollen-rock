47ae23110adf39cc48994cfee7267f57ed6d6abc
lijunsong <ljs.darkfish@gmail.com>
2017-03-01 00:30:22 -0800

    add pollen-rock command

--- info.rkt
+++ info.rkt
@@ -8,10 +8,10 @@
                "pollen"))
 
 (define build-deps '("scribble-lib" "racket-doc"))
-(define scribblings '(("scribblings/pollen-rock.scrbl" ())))
+;(define scribblings '(("scribblings/pollen-rock.scrbl" ())))
 (define pkg-desc "pollen-rock is an in-browser editor (and a server) for Pollen publishing system.")
 (define version "0.1")
 (define pkg-authors '(junsongli))
 
 (define raco-commands
-  (list '("pollen-rock" "pollen-rock/main.rkt" "launch pollen server" #f)))
+  (list '("pollen-rock" (submod pollen-rock/pollen-rock/main raco) "launch pollen server" #f)))
