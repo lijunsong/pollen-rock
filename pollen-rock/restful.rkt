5a68dfade422a8197aea7b8004b7a42e826a8c7c
lijunsong <ljs.darkfish@gmail.com>
2017-10-24 08:06:54 -0700

    restructure fs-op

--- pollen-rock/restful.rkt
+++ pollen-rock/restful.rkt
@@ -2,6 +2,7 @@
 
 (require web-server/http/request-structs)
 (require json)
+(require "config.rkt")
 (require "util.rkt")
 (require "http-util.rkt")
 (require "logger.rkt")
@@ -12,7 +13,7 @@
 ;; main-handler, and define a sub-handler accepting request and
 ;; matched-url (path elements)
 ;;
-;; NOTE: other handlers must return a Jsexpr
+;; NOTE: sub handlers must return a Jsexpr
 
 
 ;; main handler dispatch request to different handlers.
@@ -24,6 +25,7 @@
   ;;; convert ans to jsexp
   (response/text (jsexpr->bytes ans)))
 
+
 (define/contract (extract-bindings key bindings)
   (-> bytes? (listof binding?) (or/c false? bytes?))
   (let [(form (bindings-assq key bindings))]
@@ -33,29 +35,90 @@
 
 
 ;;;; POST /fs/$path
+;; file operations
+(define (fs-handler req matched-url)
+  (struct fsop (op need-data?) #:transparent)
+  ;; fs-ops - a map mapping from op names to FsOps
+  (define fs-ops
+    (hash #"mv" (fsop mv-op true)
+          #"mkdir" (fsop mkdir-op false)
+          #"rm" (fsop rm-op false)
+          #"cat" (fsop cat-op true)
+          #"ls" (fsop ls-op false)))
+  ;; prepare op arguments
+  (define src (path-elements->resource matched-url))
+  (define bindings (request-bindings/raw req))
+  (define op-name (extract-bindings #"op" bindings))
+  (define op (hash-ref fs-ops op-name false))
+  ;; run ops
+  (cond [(not op)
+         (fs-answer 1 "op field not found")]
+        [(fsop-need-data? op)
+         (let [(data (extract-bindings #"data" bindings))]
+           ((op-sanity-checker (fsop-op op)) src data))]
+        [else
+         ((op-sanity-checker (fsop-op op)) src)]))
 
+
+;; Return a function that will convert and check the arguments passed
+;; to the given func before calling it.
+(define (op-sanity-checker func)
+  (define (arg->string arg)
+    (if (bytes? arg)
+        (bytes->string/utf-8 arg)
+        arg))
+  (define (fs-op-with-handlers op)
+    (lambda args
+      (with-handlers
+          ([exn:fail:filesystem? (lambda (e) (fs-answer 2 "fs error"))])
+        (apply op args)
+        (fs-answer 0))))
+  (lambda args
+    (define string-args (map arg->string args))
+    (if (andmap non-empty-string? string-args)
+        (apply (fs-op-with-handlers func) string-args)
+        (fs-answer 1 (format "args contain non-string: ~s" string-args)))))
+
+
+;; return spec for file operation
 (define/contract (fs-answer errno [message (json-null)])
   (->* (integer?) (jsexpr?) jsexpr?)
   (hasheq 'errno errno
           'message message))
 
+(define/contract (mkdir-op src)
+  (-> string? void?)
+  (define p (append-path webroot src))
+  (log-rest-debug "mkdir-op ~a" p)
+  (make-directory p))
+
+
+(define/contract (rm-op src)
+  (-> string? void?)
+  (define p (append-path webroot src))
+  (log-rest-debug "rm-op ~a" p)
+  (delete-directory src))
+
+
 (define/contract (mv-op src dst)
   (-> string? string? jsexpr?)
-  (cond [(and (non-empty-string? dst) (non-empty-string? src))
-         (log-rest-debug "mv ~s ~s" src dst)
-         (fs-answer 0)]
-        [else
-         (fs-answer 1 (format "mv failed: mv ~s ~s" src dst))]))
+  (define src-full (append-path webroot src))
+  (define dst-full (append-path webroot dst))
+  (log-rest-debug "mv-op ~a ~a" src-full dst-full)
+  (rename-file-or-directory src-full dst-full))
 
-(define (fs-handler req matched-url)
-  ;; mv op
-  (define bindings (request-bindings/raw req))
-  (match (extract-bindings #"op" bindings)
-    [#"mv"
-     (let [(src (path-elements->resource matched-url))
-           (dst (extract-bindings #"data" bindings))]
-       (mv-op src
-              (if dst
-                  (bytes->string/utf-8 dst) "")))]
-    [else
-     (fs-answer 1 "op field doesn't exist")]))
+
+(define/contract (cat-op src data)
+  (-> string? string? jsexpr?)
+  (define p (append-path webroot src))
+  (log-rest-debug "cat-op ~a [text of length ~d]" p (string-length data))
+  (call-with-atomic-output-file p
+    (lambda (out path)
+      (display data out))))
+
+
+(define/contract (ls-op src)
+  (-> string? jsexpr?)
+  (define p (append-path webroot src))
+  (log-rest-debug "ls-op ~a" p)
+  (map path->string (directory-list p)))
