4a7431beadae347b335287f080f9f3f73ae9d3cb
lijunsong <ljs.darkfish@gmail.com>
2018-12-24 12:43:59 -0800

    sidebar folder click works
    This diff adds onClick events to folders on that sidebar,
    and adds css style to indent children

--- editor/src/Canvas.js
+++ editor/src/Canvas.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import { CM } from './Editor';
 import * as Api from './Api.js';
-
-
-/*class CanvasHeader extends Component {
-  render() {
-  }
-  }*/
+import * as Icons from './Icons.js';
+import { Map, List } from 'immutable';
+import Path from 'path';
 
 function CanvasHeader(props) {
   return (
@@ -19,13 +16,13 @@ function CanvasPanelContents(props) {
   const entries = props.entries.map((e, i) => {
     if (e.endsWith("/")) {
       return <div className="isDir entry"
-              key={i}>{e}</div>;
+                  key={i}>{Icons.arrowRight} {e}</div>;
     } else {
       return <div className="isFile entry"
-                 key={i}
-                 onClick={() => { props.onClick(e); }}>
-                {e}
-              </div>;
+                  key={i}
+                  onClick={() => { props.onClick(e); }}>
+               {e}
+             </div>;
     }
   });
 
@@ -34,16 +31,82 @@ function CanvasPanelContents(props) {
   );
 }
 
+/// The entry tab of the panel
+class CanvasPanelEntries extends Component {
+  _getFileOnClick(path) {
+    return () => this.props.fileOnClick(path);
+  }
+  _getFolderOnClick(path) {
+    return () => {
+      console.log("folder click " + path);
+      this.props.folderOnClick(path);
+    };
+  }
+  /// recursively render folders and it's children
+  _renderFolder(parentPath, folderName) {
+    const fullPath = Path.join(parentPath, folderName);
+    const folder = <span onClick={this._getFolderOnClick(fullPath)}>
+                     {Icons.arrowRight} {folderName}
+                   </span>;
+    const children = this.props.entries.get(fullPath);
+    let childrenView = "";
+    if (children) {
+      childrenView = this._renderEntryList(fullPath, children);
+    }
+    return <div className="isDir entry" key={fullPath}>
+             {folder}
+             <div className="spacer">{childrenView}</div>
+           </div>;
+  }
+
+  /// render a single file entry
+  _renderFile(parentPath, fileName) {
+    const fullPath = Path.join(parentPath, fileName);
+    return <div className="isFile entry" key={fullPath}
+                onClick={this._getFileOnClick((fullPath))}>
+             {fileName}
+           </div>;
+  }
+
+  /// Return a list of rendered entries
+  _renderEntryList(parentPath, list) {
+    return list.map((e) => {
+      if (e.endsWith("/")) {
+        return this._renderFolder(parentPath, e);
+      } else {
+        return this._renderFile(parentPath, e);
+      }
+    });
+  }
+
+  render() {
+    const parentPath = "/";
+    const list = this.props.entries.get(parentPath);
+    const renderedList = this._renderEntryList(parentPath, list);
+
+    if (renderedList.isEmpty()) {
+      renderedList = "No files";
+    }
+     return <div className="entries">{renderedList}</div>;
+  }
+}
+
 /// Render the left panel bar when Canvas is not in fullscreen
+///
+///        <CanvasPanelContents
+///          entries={props.entries}
+///          onClick={props.entryOnClick}
+///        />
 function CanvasPanel(props) {
   return (
     <div id="canvasPanel">
       <div id="canvasPanelHeader">Entries
       </div>
       <div id="canvasPanelContents">
-        <CanvasPanelContents
+        <CanvasPanelEntries
           entries={props.entries}
-          onClick={props.entryOnClick}
+          fileOnClick={props.fileOnClick}
+          folderOnClick={props.folderOnClick}
         />
       </div>
     </div>
@@ -54,7 +117,10 @@ class Canvas extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      entries: ['Loading'],
+      /// entries key is path, value is it's children, a list of string.
+      /// Same as pollen-rock fs api, if item ends with /, the item is
+      /// a folder
+      entries: Map({"/": List(["Loading"])}),
       openFile: null
     };
   }
@@ -75,8 +141,17 @@ class Canvas extends Component {
     );
   }
 
-  entryOnClick(filePath) {
+  fileOnClick(filePath) {
     this.setState({openFile: filePath});
+  }
+
+  folderOnClick(folderPath) {
+    Api.getContents(folderPath).then((res) => {
+      console.log(`set ${folderPath} in the entries map`);
+      const entries = this.state.entries.set(folderPath, List(res.data.items));
+      console.log(entries);
+      this.setState({entries});
+    });
   }
 
   renderNonFullscreen() {
@@ -88,7 +163,8 @@ class Canvas extends Component {
         <div className="sideBySideWrapper">
           <CanvasPanel
             entries={this.state.entries}
-            entryOnClick={this.entryOnClick.bind(this)}
+            fileOnClick={this.fileOnClick.bind(this)}
+            folderOnClick={this.folderOnClick.bind(this)}
           />
           <CM path={openFile} key={openFile}/>
         </div>
@@ -102,7 +178,9 @@ class Canvas extends Component {
 
   _loadEntryData() {
     Api.getContents("/").then((res) => {
-      this.setState({entries: res.data.items});
+      const oldEntries = this.state.entries;
+      const newEntries = oldEntries.set("/", List(res.data.items));
+      this.setState({entries: newEntries});
     });
   }
 
