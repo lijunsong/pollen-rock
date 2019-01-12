a9913e18ccad0939fba5bf014574a226a1bfe76f
lijunsong <ljs.darkfish@gmail.com>
2019-01-12 10:56:30 -0800

    fix a crash in pollen mode; add test case

--- editor/src/mode/pollen.js
+++ editor/src/mode/pollen.js
@@ -26,7 +26,7 @@ function mode(config) {
   let cmdChar = config['command-char'] || '◊';
   let racketId = `[^ \\n(){}\\[\\]",'\`;#|\\\\${cmdChar}]+`;
   let racketIdRegex = new RegExp(racketId);
-  let commentTag = `${cmdChar};`
+  let commentTag = `${cmdChar};`;
 
   function leftRightBraceMatch(left, right) {
     if (left === '{' && right === '}') {
@@ -43,7 +43,7 @@ function mode(config) {
   /// an item on stack contains left brace with an optional tagName
   class Stack {
     constructor() {
-      this.stack = []
+      this.stack = [];
     }
     get length() {
       return this.stack.length;
@@ -57,7 +57,7 @@ function mode(config) {
       this.stack.push({
         tag: tag,
         brace: brace
-      })
+      });
     }
     pop() {
       let top = this.top();
@@ -106,7 +106,7 @@ function mode(config) {
     }
     let matched = stream.match(racketIdRegex);
     if (matched === null) {
-      tag = stream.eat(";")
+      tag = stream.eat(";");
       return tag || null;
     }
     return matched[0];
@@ -190,7 +190,7 @@ function mode(config) {
       }
 
       let top = stack.pop();
-      if (! leftRightBraceMatch(top.brace, brace)) {
+      if (!top || ! leftRightBraceMatch(top.brace, brace)) {
         console.error("left Brace does not match the right one");
       }
 
@@ -216,137 +216,3 @@ function mode(config) {
 
 
 CodeMirror.defineMode("pollen", mode);
-
-CodeMirror.defineMode("pollen1", function(cmConfig, modeConfig) {
-  var cmdChar = cmConfig['command-char'] || '◊';
-  //var racketId = '[' +  "^ \\n(){}\\[\\]\",'`;#|\\\\" + cmdChar + ']';
-  var racketId = `[^ \\n(){}\\[\\]",'\`;#|\\\\${cmdChar}]`;
-  var racketIdReg = new RegExp(racketId);
-
-  function stackPush(stack, v) {
-    stack.push(v);
-  }
-
-  function stackPop(stack, v) {
-    return stack.pop();
-  }
-
-  function stackEmpty(stack) {
-    return stack === [];
-  }
-
-  function stackTopMatch(stack, v) {
-    return (!stackEmpty(stack)) && stack[stack.length-1] === v;
-  }
-
-  return {
-    'startState': function () {
-      return {
-        braceStack: [],  // store '|' (for |{) or '{'
-        mode: null
-      };
-    },
-
-    'token' : function (stream, state) {
-      //console.log(stream);
-      //console.log(state);
-      var ch;
-
-      if (stream.eatSpace()) {
-        return null;
-      }
-
-      // TODO: to clean this, push tag-function and its brace (either { or |{) information
-      // on the stack. if current char is in tag function ";", it is in comment.
-      // '}' in ";" with "|{" will be matched one more with "|", etc.
-      if (state.mode === 'block-comment') {
-        ch = stream.next();
-        // keep track of left brace in comment only when the comment
-        // starts with { (instead of |{)
-        if (ch === '{') {
-          if (stackTopMatch(state.braceStack, "comment{") || stackTopMatch(state.braceStack, "{")) {
-            // if previous open brace is comment{, push this brace.
-            // or if the previous is {, which means this brace is in a
-            // comment starting with {, push this brace as well.
-            stackPush (state.braceStack, '{');
-          }
-        }
-        else if (ch === '}') {
-          if (stackTopMatch(state.braceStack, "comment{")) {
-            state.mode = false;
-          }
-          // comment starts with "|{", we need it to match }|
-          else if (stackTopMatch(state.braceStack, "comment|") && stream.eat("|")) {
-            state.mode = false;
-          }
-          // always pop the stack
-          // assert (state.braceStack is not empty)
-          stackPop(state.braceStack);
-        }
-        return 'comment';
-      }
-      else {
-        ch = stream.next();
-        //console.log('char is: ' + ch);
-        if (ch === cmdChar) {
-          // @; or @;{ or @;|{
-          if (stream.eat(";")) {
-            if (stream.eat("|") && stream.eat("{")) {
-              stackPush(state.braceStack, "comment|");
-              state.mode = 'block-comment';
-            }
-            else if (stream.eat("{")) {
-              stackPush(state.braceStack, "comment{");
-              state.mode = 'block-comment';
-            }
-            else {
-              stream.skipToEnd();
-            }
-            return 'comment';
-          }
-          // @keyword
-          else {
-            // var tag = '';
-            // var letter;
-
-            if (stream.eat("|")) {
-              // racket bar quoted identifier, e.g. @|one, two|
-              // https://docs.racket-lang.org/guide/symbols.html
-              stream.eatWhile(/[^|]/);
-              stream.eat("|");
-            } else {
-              while ((/*letter = */stream.eat(racketIdReg)) != null) {
-                // tag += letter;
-              }
-            }
-            return 'keyword';
-          }
-        }
-        else if (ch === '|') {
-          if (stream.eat("{")) {
-            stackPush(state.braceStack, ch);
-          }
-        }
-        else if (ch === '{') {
-          stackPush(state.braceStack, ch);
-        }
-        else if (ch === '}') {
-          if (stackEmpty(state.braceStack)) {
-            // mismatched pair
-            return 'invalid';
-          }
-          else if (stackTopMatch(state.braceStack, "{")) {
-            stackPop(state.braceStack);
-          }
-          else if (stackTopMatch(state.braceStack, "|")) {
-            if (stream.eat("|")) {
-              stackPop(state.braceStack);
-            }
-          }
-        }
-      }
-      return null;
-    }
-  };
-
-});
